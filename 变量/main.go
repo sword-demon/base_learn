a645d23410ca1caa991914303d0834aa0c213665
wxvirus <1418667580@qq.com>
2021-07-18 11:14:37 +0800

    字符串底层原理剖析

--- 变量/main.go
+++ 变量/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
 
 const (
 	v1 = iota
@@ -16,7 +20,7 @@ func main() {
 	fmt.Println(name, &name)
 	fmt.Println(nickname, &nickname)
 
-	name = "666"
+	name = "无佩奇"
 
 	fmt.Println(name, &name)
 	fmt.Println(nickname, &nickname)
@@ -30,4 +34,33 @@ func main() {
 	}
 
 	fmt.Println(v1, v2, v3, v4)
+
+	username := "无解"
+
+	// 获取字符串的长度，字节长度
+	fmt.Println(len(username))
+
+	// 字符串转换为一个字节集合
+	byteStr := []byte(username)
+	// [230 151 160 232 167 163] 以10进制的方式进行展示
+	fmt.Println(byteStr)
+
+	// 字节的集合转换为字符串
+	byteList := []byte{230, 151, 160, 232, 167, 163}
+	targetString := string(byteList)
+	fmt.Println(targetString)
+
+	// rune的集合
+	// 将一个字符串转换为unicode字符集码点的集合
+	tempSet := []rune(name)
+	fmt.Println(tempSet) // [26080 20329 22855]
+
+	fmt.Println(tempSet[0], strconv.FormatInt(int64(tempSet[0]), 16))
+	fmt.Println(tempSet[1], strconv.FormatInt(int64(tempSet[1]), 16))
+	fmt.Println(tempSet[2], strconv.FormatInt(int64(tempSet[2]), 16))
+
+	// 长度的处理
+	runeLength := utf8.RuneCountInString(username)
+	fmt.Println(runeLength) // 2
+
 }
