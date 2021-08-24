da20b7c0256bf0b51528100c18c44df2b6fb4cb2
wxvirus <1418667580@qq.com>
2021-08-24 22:53:26 +0800

    切片的操作

--- 切片/main.go
+++ 切片/main.go
@@ -1,14 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-08-22 15:43:50
- * @LastEditTime: 2021-08-22 16:13:09
+ * @LastEditTime: 2021-08-24 21:25:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /base_learn/切片/main.go
  */
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func qiepian() {
 	v1 := []int{11, 22, 33, 44, 55, 66}
@@ -16,6 +18,50 @@ func qiepian() {
 	v2 := v1[1:3]
 	fmt.Println(v1) // [11 22 33 44 55 66]
 	fmt.Println(v2) // [22, 33]
+}
+
+func DeleteMap() {
+	v1 := []int{11, 22, 33, 44, 55, 66}
+	deleteIndex := 2
+	result := append(v1[:deleteIndex], v1[deleteIndex+1:]...)
+	fmt.Println(result)
+}
+
+func InsertMap() {
+	v1 := []int{11, 22, 33, 44, 55, 66}
+	insertIndex := 3 // 在索引3的位置插入99
+
+	/*
+		result := make([]int, 0, len(v1)+1)
+		result = append(result, v1[:insertIndex]...)
+		result = append(result, 99)
+		result = append(result, v1[insertIndex:]...)
+		fmt.Println(result)
+	*/
+
+	// 错误思维
+	result := append(v1[:insertIndex], 99)       // 相当于覆盖了v1里的44变成了99
+	result = append(result, v1[insertIndex:]...) // [11, 22, 33, 99, 99 ,55, 66]
+	fmt.Println(result)
+}
+
+func ForMap() {
+	v1 := []int{11, 22, 33, 44, 55, 66}
+	for i := 0; i < len(v1); i++ {
+		fmt.Println(i, v1[i])
+	}
+
+	for index, item := range v1 {
+		fmt.Println(index, item)
+	}
+}
+
+func ParameterEqual() {
+	v1 := []int{6, 9}
+	v2 := v1 // 变量赋值
+
+	v1[0] = 1111
+	fmt.Println(v1, v2) // 内存地址不相同，但是内部指向数据的地方是相同的
 }
 
 func main() {
